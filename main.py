d7ba5b0f4cfccf3a54aa73a14f748a3aadffdd69
RyunosukeT-coco <tahara-r@cocores.co.jp>
2017-03-31 17:21:40 +0900

    rs232c test finish

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
+import unittest
 from CanDigitalPanel import rs232c
-import time
 
 
-def main_check():
-    cdp = rs232c.rs232c(4)
-    ret = cdp.into_program_mode()
-    print(ret)
+class Test_CanDigitalPanel(unittest.TestCase):
+    CDP = rs232c.rs232c(5)
+    dic = rs232c.CDP_config
 
-    ret = cdp.CDP_setting_write(30, 0)
-    print(ret)
+    def setUp(self):
+        self.CDP.into_program_mode()
 
-    ret = cdp.CDP_setting_read(30)
-    print(ret)
+    def tearDown(self):
+        self.CDP.outof_program_mode()
 
-    ret = cdp.CDP_setting_write(30, 2)
-    print(ret)
+    def _test_setting_WriteRead(self, item, value):
+        ret_w = self.CDP.setting_write(item, value)
+        self.assertEquals(b"O\r\n", ret_w)
 
-    ret = cdp.CDP_setting_read(30)
-    print(ret)
+        ret_r = self.CDP.setting_read(item)
+        ret_r_uni = int((ret_r.decode("utf-8")).split()[0])
+        exp_r = value
+        self.assertEqual(exp_r, ret_r_uni)
 
-    cdp.CDP_setting_test(30, 0)
-    cdp.CDP_setting_test(30, 1)
-    cdp.CDP_setting_test(30, 2)
+    def _test_single_setting(self):
+        self._test_setting_WriteRead(30, 0)
+        self._test_setting_WriteRead(30, 2)
 
-    ret = cdp.outof_program_mode()
-    print(ret)
+    def test_all_setting(self):
+        keys = self.dic.config.keys()
+        for i in keys:
+            with self.subTest(i=i):
+                begin, end, float_change = self.dic.config[i]
+                for j in range(begin, end + 1):
+                    with self.subTest(j=j):
+                        # 小数変換
+                        deci = round(j * float_change, 6)
+                        self._test_setting_WriteRead(i, j)
 
-    cdp.rs232c_close()
-    return
 
-main_check()
 
+if __name__ == "__main__":
+    unittest.main()
