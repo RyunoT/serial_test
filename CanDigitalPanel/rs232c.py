0958f20f0fe1989d684a6a01812ee871851607f9
RyunosukeT-coco <tahara-r@cocores.co.jp>
2017-03-31 19:05:14 +0900

    separate tests

--- CanDigitalPanel/rs232c.py
+++ CanDigitalPanel/rs232c.py
@@ -13,25 +13,49 @@ class CDP_config:
         7: [0, 1, 1],
         10: [1, 9999, 1],
         11: [1, 9999, 1],
-        #12: [0, 6, 1],  bug!!!!!
-        12: [1, 6, 1],
+        12: [0, 6, 1],  # bug!!!!!
         13: [0, 1, 1],
         14: [0, 1, 1],
         15: [0, 1, 1],
         16: [0, 1, 1],
         17: [0, 1, 1],
-        #20: [1, 99999, 1],
-        #21: [1, 99999, 1],
-        #22: [1, 99999, 1],
-        #23: [1, 99999, 1],
+        20: [1, 99999, 1],
+        21: [1, 99999, 1],
+        22: [1, 99999, 1],
+        23: [1, 99999, 1],
         30: [0, 3, 1],
+    }
+
+    initial_config = {
+        1: 0,
+        2: 1,
+        3: 0.3,
+        4: 1,
+        5: 2,
+        6: 25,
+        7: 0,
+        10: 300,
+        11: 700,
+        12: 5,
+        13: 1,
+        14: 1,
+        15: 0,
+        16: 0,
+        17: 0,
+        20: 20000,
+        21: 20000,
+        22: 1,
+        23: 1,
+        30: 0,
     }
 
 
 class rs232c:
-    def __init__(self, port_num):
+    config = CDP_config()
+
+    def __init__(self, port_num, boudrate):
         self.ser = serial.Serial(port=port_num - 1,
-                                 baudrate=115200,
+                                 baudrate=boudrate,
                                  bytesize=8,
                                  parity="N",
                                  stopbits=1,
@@ -71,3 +95,8 @@ class rs232c:
         self._rs232c_write(line)
         return self.read_buff
 
+    def initial_setting(self):
+        initial_keys = self.config.initial_config.keys()
+        for i in initial_keys:
+            initial_value = self.config.initial_config[i]
+            self.setting_write(i, initial_value)
