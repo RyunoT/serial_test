e60b8584e134a65b8f70557b52374f33c48a52b6
RyunosukeT-coco <tahara-r@cocores.co.jp>
2017-03-31 17:34:03 +0900

    rs232c test finish

--- CanDigitalPanel/rs232c.py
+++ CanDigitalPanel/rs232c.py
@@ -1,5 +1,4 @@
 import serial
-import math
 
 
 class CDP_config:
@@ -71,29 +70,4 @@ class rs232c:
         line = "WP" + str(item).zfill(2) + "," + str(value) + "\r"
         self._rs232c_write(line)
         return self.read_buff
-
-    def _setting_WriteRead(self, item, value):
-        """writing test"""
-        ret_w = self.setting_write(item, value)
-        if not ret_w == b"O\r\n":
-            #print("test failed {0}".format(self.write_buff))
-            pass
-
-        """reading test"""
-        ret_r = self.setting_read(item)
-        if not ret_r == (str(value) + "\r\n").encode("utf-8"):
-            #print("test failed {0}".format(self.write_buff))
-            pass
-
-    def _all_CDP_setting_test(self):
-        keys = CDP_config.config.keys()
-        for i in keys:
-            begin, end, float_change = CDP_config.config[i]
-            for j in range(begin, end + 1):
-                # 小数変換
-                deci = round(j * float_change, 6)
-                self._setting_WriteRead(i, j)
-            # print("Finish WP{0} and RP{0} test".format(str(i).zfill(2)))
-
-        print("Finish all_setting_test")
 
